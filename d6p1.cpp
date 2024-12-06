d3dcadbf6e5d4c0205d97b296c873c1b1dd6cc9d
kjljixx <76906955+kjljixx@users.noreply.github.com>
2024-12-06 20:31:15 +0000

    day 6

---
+++ d6p1.cpp
@@ -0,0 +1,99 @@
+#include <array>
+#include <fstream>
+#include <iostream>
+#include <set>
+#include <vector>
+
+struct Pos{
+    int x;
+    int y;
+    int dir;
+    bool outside;
+};
+
+std::array<int, 4> xChange = {{0, 1, 0, -1}};
+std::array<int, 4> yChange = {{-1, 0, 1, 0}};
+
+void move(std::vector<std::vector<bool>>& isObstacle, Pos& pos){
+    if(pos.x + xChange[pos.dir] >= isObstacle.size() || pos.x + xChange[pos.dir] < 0){
+        pos.outside = true;
+        return;
+    }
+    if(pos.y + yChange[pos.dir] >= isObstacle.size() || pos.y + yChange[pos.dir] < 0){
+        pos.outside = true;
+        return;
+    }
+
+    int newX = pos.x + xChange[pos.dir];
+    int newY = pos.y + yChange[pos.dir];
+    // std::cerr << newY << " " << newX << std::endl;
+    if(isObstacle[newY][newX]){
+        pos.dir += 1;
+        pos.dir %= 4;
+        return;
+    }
+
+    pos.x = newX;
+    pos.y = newY;
+    return;
+}
+
+int main(){
+    std::ifstream inputFile("input.txt");
+    std::string line;
+
+    std::vector<std::vector<bool>> isObstacle;
+    Pos pos; pos.outside = false;
+
+    int y = 0;
+    while(std::getline(inputFile, line)){
+        // std::cerr << "hi";
+        isObstacle.push_back(std::vector<bool>());
+        int x = 0;
+        for(char c : line){
+            if(c == '#'){
+                isObstacle[y].push_back(true);
+            }
+            else{
+                isObstacle[y].push_back(false);
+            }
+            if(c == '^'){
+                pos.x = x;
+                pos.y = y;
+                pos.dir = 0;
+            }
+            if(c == '>'){
+                pos.x = x;
+                pos.y = y;
+                pos.dir = 1;
+            }
+            if(c == 'v'){
+                pos.x = x;
+                pos.y = y;
+                pos.dir = 2;
+            }
+            if(c == '<'){
+                pos.x = x;
+                pos.y = y;
+                pos.dir = 3;
+            }
+            x++;
+        }
+        y++;
+    }
+
+
+    std::set<std::pair<int, int>> visited;
+    int ans = 1;
+    visited.insert({pos.x, pos.y});
+    while(!pos.outside){
+        move(isObstacle, pos);
+        // std::cerr << isObstacle.size() << " " << pos.x << " " << pos.y << " " << pos.dir << " " <<pos.outside << std::endl;
+        if(visited.find({pos.x, pos.y}) == visited.end()){
+            ans++;
+            visited.insert({pos.x, pos.y});
+        }
+    }
+
+    std::cout << ans << std::endl;
+}
