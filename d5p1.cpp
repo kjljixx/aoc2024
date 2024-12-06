213bc0970c61f964a68cc54104333cd42cc98ee6
kjljixx <76906955+kjljixx@users.noreply.github.com>
2024-12-06 20:33:20 +0000

    day 5

---
+++ d5p1.cpp
@@ -0,0 +1,63 @@
+#include <fstream>
+#include <iostream>
+#include <map>
+#include <vector>
+
+bool isFollowsRules(std::vector<int>& pages, std::map<int, std::vector<int>>& rules){
+    std::map<int, int> pagesMap;
+    for(int i=0; i<pages.size(); i++){
+        pagesMap[pages[i]] = i;
+    }
+    for(int i=0; i<pages.size(); i++){
+        int pageNum = pages[i];
+        for(int laterPage : rules[pageNum]){
+            // std::cout << pageNum << " " << laterPage << std::endl;
+            if(pagesMap.find(laterPage) != pagesMap.end() && pagesMap[laterPage] < i){
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+int main(){
+    std::string line;
+    std::ifstream inputFile("input.txt");
+    
+    std::vector<int> pages;
+    std::map<int, std::vector<int>> rules;
+
+    while(true){
+        std::getline(inputFile, line);
+
+        int splitPos = line.find('|');
+        if(splitPos == std::string::npos){
+            // std::cerr << "hi";
+            break;
+        }
+
+        rules[std::stoi(line.substr(0, splitPos))].push_back(std::stoi(line.substr(splitPos+1)));
+    }
+
+    int ans = 0;
+    while(std::getline(inputFile, line)){
+    // std::cerr << line << "\n"; 
+        pages.clear();
+        while(true){
+            int splitPos = line.find(',');
+            if(splitPos == std::string::npos){
+                break;
+            }
+
+            pages.push_back(std::stoi(line.substr(0, splitPos)));
+            line = line.substr(splitPos+1);
+        }
+        pages.push_back(std::stoi(line));
+
+        // std::cout << "hi" << std::endl;
+        if(isFollowsRules(pages, rules)){
+            ans += pages[pages.size() / 2];
+        }
+    }
+    std::cout << ans << std::endl;
+}
